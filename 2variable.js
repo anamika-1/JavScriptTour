fbfc72ce81eda523fbf102b41b641dc8e7102d42
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-02-23 21:29:39 +0530

    Create 2variable.js

---
+++ 2variable.js
@@ -0,0 +1,12 @@
+const accountId = 13122
+let accountEmail = "ana@gmail.com"
+var accountPassword = "7435783"
+accountCity = "hadfh"
+
+// accountId = 6171 ---not allowed bcz it is constant
+
+accountEmail = "abcdafads@gmail.com"
+accountPassword = "265126"
+accountCity = "jahfj"
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
