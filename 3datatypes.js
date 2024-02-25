08e357356f4f7f747ce4ecc3d5a7f7a62fbf5812
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-02-25 13:31:06 +0530

    Create 3datatypes.js

---
+++ 3datatypes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/* 
+-> primitive datatypes in js are----
+1.null = standalone value //typeof null = object
+2.undefined = declared but not initialized // typeof undefined = undefined
+3.boolean = true/false
+4.number = have specific range
+5.bigint = for very large int
+6.string = "text value"
+7.symbol = unique value
+
+-> user defined datatype in js-----
+1. objects
+....some more, will discover them later
+*/
+
+let id = null
+let roll = undefined
+let attend = true
+let age = 21
+let salary = 8384294 //🤭
+let userName = "Anamika"
+//let symbol 
+
+console.table([id, roll, userName, age, attend, salary])
