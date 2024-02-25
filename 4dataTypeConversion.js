0879f161c4803c673a1cf88d5921d516d9826607
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-02-25 19:13:57 +0530

    Create 4dataTypeConversion.js

---
+++ 4dataTypeConversion.js
@@ -0,0 +1,44 @@
+//number
+let score = 12
+console.log("Score: " + score)
+console.log("Type of score: " + typeof(score))
+
+let valueInNum = Number(score)
+console.log("valueInNum: " + valueInNum)
+console.log("Type of valueInNum: " + typeof(valueInNum))
+
+//undefined
+// let score = undefined
+// console.log("Score: " + score)
+// console.log("Type of score: " + typeof(score))
+
+// let valueInNum = Number(score)
+// console.log("valueInNum: " + valueInNum)
+// console.log("Type of valueInNum: " + typeof(valueInNum))
+
+//null
+// let score = null
+// console.log("Score: " + score)
+// console.log("Type of score: " + typeof(score))
+
+// let valueInNum = Number(score)
+// console.log("valueInNum: " + valueInNum)
+// console.log("Type of valueInNum: " + typeof(valueInNum))
+
+//string
+// let score = "Anamika"
+// console.log("Score: " + score)
+// console.log("Type of score: " + typeof(score))
+
+// let valueInNum = Number(score)
+// console.log("valueInNum: " + valueInNum)
+// console.log("Type of valueInNum: " + typeof(valueInNum))
+
+// boolean 
+// let score = false
+// console.log("Score: " + score)
+// console.log("Type of score: " + typeof(score))
+
+// let valueInNum = Number(score)
+// console.log("valueInNum: " + valueInNum)
+// console.log("Type of valueInNum: " + typeof(valueInNum))
