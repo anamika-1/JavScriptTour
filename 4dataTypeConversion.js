258fb6a2ca01cd20b1e0ae9139ab3d4b28b54b33
Anamika Jha <67423289+anamika-1@users.noreply.github.com>
2024-02-26 17:47:07 +0530

    Update 4dataTypeConversion.js

--- 4dataTypeConversion.js
+++ 4dataTypeConversion.js
@@ -53,3 +53,61 @@ console.log("Type of valueInNum: " + typeof(valueInNum))
 // let booleanIsLoggedIn = String(isLoggedIn)
 // console.log("boolenIsLoggedIn: " + booleanIsLoggedIn)
 // console.log("Type of boolenIsLoggedIn: " + typeof(booleanIsLoggedIn))
+
+
+
+
+
+
+// *************Operations******************
+
+
+// let negval1 = -val1
+// console.log(negval1)
+
+// let val1 = 3
+// let val2 = 5
+
+// console.log(val2 + val1)//addition
+// console.log(val2 - val1)//subtraction
+// console.log(val2 * val1)//multiplication
+// console.log(val2 ** val1)//power
+// console.log(val2 / val1)//division
+// console.log(val2 % val1)//module
+
+
+// string operations
+
+// str1 = "hello "
+// str2 = "anamika"
+// console.log(str1 + str2);
+
+
+//-> string with numbers
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3); 
+// console.log(1 + 2 + "3");
+
+//-> boolean 
+// console.log(true);
+// console.log(+true);
+// // console.log(true+)//error
+// console.log(false);
+// console.log(+false);
+
+// console.log(+" ");
+
+
+// Increment & Decrement Operator 
+
+// let counter = 100;
+// ++counter //pre increment
+// console.log(counter);
+// counter++ //post increment
+// console.log(counter);
+
+// --counter //pre decrement
+// console.log(counter);
+// counter-- //post decrement
+// console.log(counter);
